fef1b21140766e2b2c2ceac8e67e63f81b01769a
FoxIshDaBest <foxishdabest@gmail.com>
2016-02-20 20:43:54 -0800

    Update Command_freedombot.java

--- src/com/FoxIshDaBest/FreedomBot/Commands/Command_freedombot.java
+++ src/com/FoxIshDaBest/FreedomBot/Commands/Command_freedombot.java
@@ -17,14 +17,14 @@ public class Command_freedombot implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String string, String[] args) {
         
-        sender.sendMessage(ChatColor.DARK_GRAY + "-----" + ChatColor.DARK_RED + "FreedomBot v1.2" + ChatColor.DARK_GRAY + "-----");
+        sender.sendMessage(ChatColor.DARK_GRAY + "-----" + ChatColor.DARK_RED + "FreedomBot v1.3" + ChatColor.DARK_GRAY + "-----");
         sender.sendMessage(ChatColor.YELLOW + "You are running " + ChatColor.GOLD + ChatColor.BOLD + "FreedomBot v1.3!");
         sender.sendMessage(ChatColor.YELLOW + "FreedomBot is a FreeOp server bot that makes admin's jobs easier.");
-        sender.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Commands: " + ChatColor.YELLOW + "/freedombot, /opme, /adminhelp");
+        sender.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Commands: " + ChatColor.YELLOW + "/freedombot, /opme, /admintool, /adminalert");
         sender.sendMessage(ChatColor.YELLOW + "More features coming soon!");
         sender.sendMessage(ChatColor.YELLOW + "Want to help with the development of the FreedomBot?");
-        sender.sendMessage(ChatColor.YELLOW + "Go to https://github.com/RainbowFreedomDev/FreedomBot!");
-        sender.sendMessage(ChatColor.DARK_GRAY + "-----" + ChatColor.DARK_RED + "FreedomBot v1.2" + ChatColor.DARK_GRAY + "-----");
+        sender.sendMessage(ChatColor.YELLOW + "Go to https://github.com/SecreticOP/FreedomBot!");
+        sender.sendMessage(ChatColor.DARK_GRAY + "-----" + ChatColor.DARK_RED + "FreedomBot v1.3" + ChatColor.DARK_GRAY + "-----");
 
         return true;
 
