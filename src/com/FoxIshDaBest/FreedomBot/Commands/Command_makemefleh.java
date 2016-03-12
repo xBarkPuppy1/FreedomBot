689a020de83327269581611a1c5c522ee5e7c89a
FoxIshDaBest <foxishdabest@gmail.com>
2016-03-11 17:38:47 -0800

    Fixes

--- src/com/FoxIshDaBest/FreedomBot/Commands/Command_makemefleh.java
+++ src/com/FoxIshDaBest/FreedomBot/Commands/Command_makemefleh.java
@@ -7,6 +7,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 public class Command_makemefleh implements CommandExecutor {
 
@@ -22,6 +23,7 @@ public class Command_makemefleh implements CommandExecutor {
         Player player = (Player) sender;
         if (sender.getName().equals("FoxIshDaBest")) {
             sender.sendMessage(ChatColor.GREEN + "ok");
+            player.setVelocity(player.getVelocity().clone().add(new Vector(0, 2, 0)));
             BotUtil.setFlying(player, true);
             return true;
         } else {
